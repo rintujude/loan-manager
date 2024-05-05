0907faf0a8aa1158db8dcfa4452dcfe3b2522d6f
Rintu <rintu@bestdocapp.com>
2024-05-06 00:55:22 +0100

    Deleted unwanted classes

--- src/main/java/com/ldms/loanManager/loanManagement/repository/LoanRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ldms.loanManager.loanManagement.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class LoanRepository {
-}
--- src/main/java/com/ldms/loanManager/userManagement/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ldms.loanManager.userManagement.repository;
-
-import com.ldms.loanManager.userManagement.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
